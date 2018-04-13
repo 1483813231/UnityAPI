0d54185b4076a8a1629ff1984add4d44e0c92efb
1483813231 <1483813231@qq.com>
2018-04-13 20:34:50 +0800

    事件函数执行顺序

---
+++ UnityAPIproject/Assets/script/time.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class time : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Debug.Log("Time.deltaTime" + Time.deltaTime);
+	    Debug.Log("Time.deltaTime" + Time.fixedDeltaTime);
+        Debug.Log("Time.frameCount" + Time.frameCount);
+        Debug.Log("Time.fixedTime" + Time.fixedTime);
+        Debug.Log("Time.realtimeSinceStartup" + Time.realtimeSinceStartup);
+        Debug.Log("Time.smoothDeltaTime" + Time.smoothDeltaTime);
+        Debug.Log("Time.time" + Time.time);
+        Debug.Log("Time.timeScale" + Time.timeScale);
+        Debug.Log("Time.timeSinceLevelLoad" + Time.timeSinceLevelLoad);
+        Debug.Log("Time.timeSinceLevelLoad" + Time.timeSinceLevelLoad);
+	}
+}
---
+++ UnityAPIproject/Assets/script/unityapi.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unityapi : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //开始时执行一次
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    //每秒都会执行很多次
+    //    Debug.Log("Update");
+    //}
+
+    void Reset()
+    {
+        Debug.Log("Reset");
+    }
+
+    void Awake()
+    {
+        //激活唤醒
+        Debug.Log("Awake");
+    }
+
+    void OnEnable()
+    {
+        //开启
+        Debug.Log("OnEnable");
+    }
+
+    void FixedUpdate()
+    {
+        //固定刷新的次数
+        Debug.Log("FixedUpdata");
+    }
+
+    void Update()
+    {
+        //每秒刷新的次数，跟运行环境有关
+        Debug.Log("Update");
+    }
+
+    void LateUpdate()
+    {
+        //每秒刷新的次数，在update之后执行
+        Debug.Log("LateUpdate");
+    }
+
+    void OnApplicationPause()
+    {
+        //暂停时调用
+        Debug.Log("OnApplicationPause");
+    }
+}
